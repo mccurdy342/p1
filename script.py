cfb12d7dd24e4b803ad5a324573d5129b01b982d
Sarah McCurdy <s2mccurdy@edu.uwaterloo.ca>
2021-10-02 22:03:47 -0700

    adding attractions functionality

--- script.py
+++ script.py
@@ -17,11 +17,40 @@ def get_traveler_location(traveler):
 test_destination_index = get_traveler_location(test_traveler)
 
 def add_attraction(destination, attraction):
-    destination_index = get_destination_index(destination)
-    attractions_for_destination= []
-    attactions_for_destination.append(attractions[destination_index])
+  destination_index = get_destination_index(destination)
 
-  
-#print(test_destination_index)
+  attractions_for_destination = attractions[destination_index]
 
-print(attractions)
+  attractions_for_destination.append(attraction)
+  return
+
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interests = []
+  for possible_attraction in attractions_in_city:
+    attraction_tags = possible_attraction[1]
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interests.append(interest)
+  return attractions_with_interests
+
+
+
+add_attraction("Los Angeles, USA", ['venice Beach', ['beach']])
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+#print(attractions)
+
+la_arts = find_attractions("Los Angeles, USA", ['art'])
+
+print(la_arts)
